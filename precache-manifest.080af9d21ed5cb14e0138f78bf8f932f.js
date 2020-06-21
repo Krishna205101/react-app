81d428b7dd7bf643912e173721c14fe6576f9780
Krishna205101 <krishna.koppera@gmail.com>
2020-06-21 20:47:27 +0530

    Updates

---
+++ precache-manifest.080af9d21ed5cb14e0138f78bf8f932f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd6cbb9dc42a371006f28b27178ec9d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "50d836ca834588769064",
+    "url": "/react-app/static/css/main.9fbfc4b0.chunk.css"
+  },
+  {
+    "revision": "434b95c8acb055595512",
+    "url": "/react-app/static/js/2.741f329a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.741f329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d836ca834588769064",
+    "url": "/react-app/static/js/main.e37ee630.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  },
+  {
+    "revision": "49063bfa9a9d97d8bac15c07f9284f20",
+    "url": "/react-app/static/media/images.49063bfa.png"
+  }
+]);
